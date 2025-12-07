fe92fce345707582430a34aea8712847ce0fbf62
Toby-top <damudongni40@gmail.com>
2025-12-07 16:42:11 +0800

    Add waline configuration file

---
+++ waline.config.js
@@ -0,0 +1,8 @@
+export default {
+  serverURL: "https://wdEEcZX9UHzdElzYDDNMeQZk-MdYXbMMI.api.lncldglobal.com",
+  leancloud: {
+    appId: "wdEEcZX9UHzdElzYDDNMeQZk-MdYXbMMI",
+    appKey: "8dGuUX6qyli9GIV7ZyRQtccA",
+    serverURL: "https://wdEEcZX9UHzdElzYDDNMeQZk-MdYXbMMI.api.lncldglobal.com"
+  }
+};
